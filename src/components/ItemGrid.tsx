7cadff51cc6e531b94cb8abe5bc7b24adf28469c
Anson Lee <anson.lee73@outlook.com>
2022-05-05 01:02:43 -0400

    draft of listings component

--- src/components/ItemGrid.tsx
+++ src/components/ItemGrid.tsx
@@ -1,20 +1,37 @@
-import { Box, Flex, Grid, GridItem, Link, Popover, PopoverTrigger, useColorModeValue } from '@chakra-ui/react'
+import { Box, Stack, Flex, Wrap, WrapItem, useColorModeValue, Button } from '@chakra-ui/react'
+import Image from 'next/image';
+import Link from "next/link";
 
 export const ItemGrid = () => (
-  <Box>
   <Flex 
     bg={useColorModeValue('white', 'gray.800')}
-    color={useColorModeValue('gray.600', 'white')}
-    direction="column"  
+    color={useColorModeValue('gray.600', 'white')} 
+    justifyContent={'center'} 
     py={{ base: 4 }}
     px={{ base: 4 }}
-    borderTop={1}
-    borderBottom={1}
-    borderStyle={'solid'}
-    borderColor={useColorModeValue('gray.200', 'gray.700')}
   >
+    <Wrap spacing='10px'>
+      <WrapItem>
+        <Link href={'#'}>
+          <Box w='180px' h='180px' _hover={{ bg: 'blue.500' }}>
+            <Flex direction='column' h='100%' justifyContent='center' alignItems='center'>
+              <Image src='/items/battle/stat/dire-hit.png' alt="Repel" width='30px' height='30px' />
+              <h1>label</h1>
+              <h1>price</h1>
+            </Flex>
+          </Box>
+        </Link>
+      </WrapItem>
+      <WrapItem>
+        <Link href={'#'}>
+          <Button w='180px' h='180px' variant='ghost'>
+            box3
+          </Button>
+        </Link>
+      </WrapItem>
+    </Wrap>
+    {/* 
     <Grid templateColumns='repeat(5, 1fr)' gap={6}>
-      {/*<GridItem w='100%' h='40' bg='blue.500' />*/}
       {GRID_ITEMS.map((gridItem) => (
         <GridItem key={gridItem.label} w='100%' h='40' bg='blue.500'>
           <Link>
@@ -23,8 +40,8 @@ export const ItemGrid = () => (
         </GridItem>
       ))}
     </Grid>
+    */}
   </Flex>
-  </Box>
 )
 
 interface GridItem {
